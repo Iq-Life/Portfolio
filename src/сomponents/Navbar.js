05d54acab0dade95513a8070bd14d8257ac39050
Pavel <73390739+Iq-Life@users.noreply.github.com>
2021-10-22 13:36:39 +0300

    small navbar

--- src/сomponents/Navbar.js
+++ src/сomponents/Navbar.js
@@ -11,16 +11,29 @@ export const Navbar = () => {
     return (
         <ul className={s.nav} >
             <li className={s.li}>
-                <NavLink to='/home' className={s.Link} activeClassName={s.activeLink}> <img src={home} /> <h3 className={s.title}>home</h3></NavLink>
+                <NavLink to='/home' className={s.Link} activeClassName={s.activeLink}>
+                    <img src={home} alt={"home"}/>
+                    <h3 className={s.title}>home</h3>
+                </NavLink>
             </li>
             <li className={s.li}>
-                <NavLink to='/about' className={s.Link} activeClassName={s.activeLink}><img src={about} /><h3 className={s.title}>about</h3></NavLink>
+                <NavLink to='/about' className={s.Link} activeClassName={s.activeLink}>
+                    <img src={about}  alt={"about"}/>
+                    <h3 className={s.title}>about</h3>
+
+                </NavLink>
             </li>
             <li className={s.li}>
-                <NavLink to='/projects' className={s.Link} activeClassName={s.activeLink}><img src={portfolio} /><h3 className={s.title}>portfolio</h3></NavLink>
+                <NavLink to='/projects' className={s.Link} activeClassName={s.activeLink}>
+                    <img src={portfolio} alt={"portfolio"}/>
+                    <h3 className={s.title}>portfolio</h3>
+                </NavLink>
             </li>
             <li className={s.li}>
-                <NavLink to='/contact' className={s.Link } activeClassName={s.activeLink}><img src={contact} /><h3 className={s.title}>contact</h3></NavLink>
+                <NavLink to='/contact' className={s.Link } activeClassName={s.activeLink}>
+                    <img src={contact} alt={"contact"}/>
+                    <h3 className={s.title}>contact</h3>
+                </NavLink>
             </li>
 
         </ul>
